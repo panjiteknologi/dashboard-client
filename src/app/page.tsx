b3c0e86e248218576c5a31dafc94a1566574e5fb
panjiteknologi <itserv.panji@gmail.com>
2025-05-05 13:41:41 +0700

    handle auth page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import HeroSection from "@/components/hero";
 import { Navbar } from "@/components/navbar";
 import MainLayout from "@/layout/main-layout";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/apps");
+  }
+
   return (
     <MainLayout>
       <Navbar />
