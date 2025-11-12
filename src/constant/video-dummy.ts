11f7ed7ce0e64df4c7ac40c8f2a7cbd60cca0492
panjiteknologi <itserv.panji@gmail.com>
2025-11-12 17:07:31 +0700

    update library dan survaillance

--- src/constant/video-dummy.ts
+++ src/constant/video-dummy.ts
@@ -130,6 +130,36 @@ export const dummyVideoData: VideoCategory[] = [
             title: "Manajemen Kerentanan Teknis",
             description: "Animasi Keamanan Informasi #4",
             url: "https://www.youtube.com/embed/TevqOnw0qa0?si=LmziujYSZ__-zRMx",
+          },
+          {
+            id: 13,
+            title: "Penerapan ISO 27001 Sebagai Sistem Pengelolaan Keamanan Informasi",
+            description: "Animasi Keamanan Informasi #1",
+            url: "https://www.youtube.com/embed/4l7xxWfQSJo?si=FT5t-hB-K01g_N0v",
+          },
+          {
+            id: 14,
+            title: "Membatasi akses pengguna ke situs web",
+            description: "Animasi Keamanan Informasi #7",
+            url: "https://www.youtube.com/embed/qoEksnwFPGQ?si=APRs-mjRxbncqT1G",
+          },
+          {
+            id: 15,
+            title: "Pengujian keamanan dalam pengembangan dan penerimaan",
+            description: "Animasi Keamanan Informasi #8",
+            url: "https://www.youtube.com/embed/HZQ6PDA9C_c?si=fN3IQzCjDKr2ZHvm",
+          },
+          {
+            id: 16,
+            title: "Remote working",
+            description: "Animasi Keamanan Informasi #11",
+            url: "https://www.youtube.com/embed/5QtgMrkET3Q?si=wrQm1RWcJd4TXUn4",
+          },
+          {
+            id: 17,
+            title: "Security of network services",
+            description: "Animasi Keamanan Informasi #12",
+            url: "https://www.youtube.com/embed/4dEhx6MJ2rI?si=vp2B585EfALW7hUQ",
           }
         ],
       },
